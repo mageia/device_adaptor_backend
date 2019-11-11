7bf78ca416adb88ac26e0f416e6567b76f0c9270
Mageia <yzg963@gmail.com>
2019-11-11 20:34:41 +0800

    update vendor

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -7,7 +7,7 @@ import (
 	_ "device_adaptor/plugins/inputs/http_listener"
 	_ "device_adaptor/plugins/inputs/kj66"
 	_ "device_adaptor/plugins/inputs/modbus"
-	_ "device_adaptor/plugins/inputs/opc"
+	//_ "device_adaptor/plugins/inputs/opc"
 	//_ "device_adaptor/plugins/inputs/opc_ws"
 	_ "device_adaptor/plugins/inputs/s7"
 	_ "device_adaptor/plugins/inputs/serial"
